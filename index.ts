e40be9792fade887bb2700bb02e23e4824cef021
SalimBK1993 <salim@digitalfractal.com>
2021-07-24 12:23:06 -0600

    init of app

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import express from 'express';
+const app = express();
+const port = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
