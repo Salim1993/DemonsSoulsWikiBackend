1d9a18980a605842cc6d998ac1467d4754cad379
SLgeneration <slgeneration93@gmail.com>
2021-09-17 17:52:54 -0600

    add readme

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const port = 8000;
 
 createConnection().then(connection => {
 
-  const weaponRepository = connection.getRepository(Weapon)
+  const weaponRepository = connection.getRepository(Weapon);
 
   app.get('/weapon', async (req: Request, res: Response) => {
     const weapons = await weaponRepository.find();
---
+++ readme.md
@@ -0,0 +1,7 @@
+## About
+
+Backend to send Demon souls wiki info to frontend devices. 
+
+## Initialize
+
+Clone the repo, install the required node modules using `npm install`, and finally run `npm start` 
